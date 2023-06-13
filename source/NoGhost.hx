1db2b80f5092b13b37ae351567763b35ee0b16e1
Dxgamer7405 <miguelarca7405@gmail.com>
2023-06-12 20:19:15 -0400

    porcaria velha

--- source/NoGhost.hx
+++ source/NoGhost.hx
@@ -1,12 +1,7 @@
 @:headerCode('
-#include "windows.h"
-#include "winuser.h"
 ')
 @:unreflective
 @:nativeGen
 class NoGhost {
-	@:functionCode('
-	DisableProcessWindowsGhosting();
-	')
-	public static function disable():Void {}
+    public static function disable():Void {}
 }
